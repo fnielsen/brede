8ed7b6cabf473e769a62b35a8355801904bf94cd
Finn Aarup Nielsen <faan@dtu.dk>
2014-12-17 01:08:46 +0100

    Added EegRun.

--- brede/eeg/__init__.py
+++ brede/eeg/__init__.py
@@ -1,7 +1,11 @@
 """EEG modules, classes and functions."""
 
+from __future__ import absolute_import
+
+
 from . import plotting
+from .core import EegRun
 from .plotting import TopoPlot, topoplot
 
 
-__all__ = ('TopoPlot', 'plotting', 'topoplot')
+__all__ = ('EegRun', 'TopoPlot', 'plotting', 'topoplot')
