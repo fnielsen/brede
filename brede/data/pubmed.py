9a2a6fc8afa93ba2c97a779365187d0d0ef238d3
Finn Aarup Nielsen <faan@dtu.dk>
2015-01-07 01:29:11 +0100

    Renamed method, implemented multiple medline get. Pause in download

--- brede/data/pubmed.py
+++ brede/data/pubmed.py
@@ -1,16 +1,15 @@
 """Interface to Pubmed information."""
 
-
 import errno
-
 import os
+import time
+from datetime import datetime
 from os.path import expanduser, join
 
 try:
     import cPickle as pickle
 except ImportError:
     import pickle
-
 
 from Bio import Entrez, Medline
 
@@ -19,14 +18,24 @@ from ..config import config
 
 class Pubmed(object):
 
-    """Interface to Pubmed bibliographic information."""
+    """Interface to Pubmed bibliographic information.
+
+    PubMed data in the form of MEDLINE records are cache on the local
+    filesystem as pickle data in the data directory. If the record is not
+    available locally it is fetch via the Internet on the Entrez server.
+    """
+
+    last_download_time = datetime.now()
 
     def __init__(self):
         """Setup directories."""
         self.data_dir = expanduser(config.get('data', 'data_dir'))
         self.pubmed_dir = join(self.data_dir, 'pubmed')
         self.setup_data_dir()
+        self.pause_between_downloads = 0.334
+
         Entrez.email = config.get('bio', 'email')
+        Entrez.tool = 'brede'
 
     def setup_data_dir(self):
         """Create pubmed data directory if not exists."""
@@ -39,6 +48,13 @@ class Pubmed(object):
 
     def download_medline(self, pmid):
         """Download bibliographic information from PubMed.
+
+        Note the guideline (http://www.ncbi.nlm.nih.gov/books/NBK25497/):
+
+        "In order not to overload the E-utility servers, NCBI recommends
+        that users post no more than three URL requests per second and
+        limit large jobs to either weekends or between 9:00 PM and
+        5:00 AM Eastern time during weekdays."
 
         Parameters
         ----------
@@ -58,8 +74,17 @@ class Pubmed(object):
         'Right temporoparietal cortex'
 
         """
+        # Pause to not overload Entrez server.
+        interval = (datetime.now() - Pubmed.last_download_time).total_seconds()
+        pause = max(0, self.pause_between_downloads - interval)
+        time.sleep(pause)
+
+        # Actual download
         handle = Entrez.efetch(db="pubmed", id=str(pmid),
                                rettype='medline', retmode='text')
+
+        Pubmed.last_download_time = datetime.now()
+
         records = list(Medline.parse(handle))
         if len(records) > 0:
             record = records[0]
@@ -67,7 +92,7 @@ class Pubmed(object):
             record = None
         return record
 
-    def get_pmid(self, pmid):
+    def get_medline(self, pmid):
         """Return bibliographic information from PubMed.
 
         Parameters
@@ -77,13 +102,13 @@ class Pubmed(object):
 
         Returns
         -------
-        record : Bio.Medline.Record
+        medline : Bio.Medline.Record
             Dict-like structure with bibliographic fields
 
         Example
         -------
         >>> pubmed = Pubmed()
-        >>> paper = pubmed.get_pmid(15238438)
+        >>> paper = pubmed.get_medline(15238438)
         >>> paper['TI'][:28]
         'Right temporoparietal cortex'
 
@@ -97,6 +122,23 @@ class Pubmed(object):
             else:
                 raise
         return medline
+
+    def get_medlines(self, pmids):
+        """Return MEDLINE information from several PMIDs.
+
+        Parameters
+        ----------
+        pmids : list of int or list of str
+            PMID identifiers for the PubMed database
+
+        Returns
+        -------
+        medlines : list of Bio.Medline.Record
+            List of dict-like objects with bibliographic data
+
+        """
+        medlines = [self.get_medline(pmid) for pmid in pmids]
+        return medlines
 
     def medline_filename(self, pmid):
         """Return full filename for local MEDLINE file."""
