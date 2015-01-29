c91cfc35dcd85238074fe63e93e0e54babd0541c
Finn Aarup Nielsen <faan@dtu.dk>
2015-01-29 02:14:08 +0100

    Initial version.

--- brede/data/__init__.py
+++ brede/data/__init__.py
@@ -1,8 +1,12 @@
 """Interface to data."""
 
 
-import neurosynth
-from neurosynth import NeurosynthDatabase
+from brede.data import eeg, neurosynth, pubmed, wikipedia
+from brede.data.neurosynth import NeurosynthDatabase
+from brede.data.pubmed import Pubmed
+from brede.data.wikipedia import WikiPage
 
 
-__all__ = ('neurosynth', NeurosynthDatabase)
+__all__ = ('eeg',
+           'neurosynth', 'NeurosynthDatabase',
+           'pubmed', 'Pubmed', 'wikipedia', 'WikiPage')
