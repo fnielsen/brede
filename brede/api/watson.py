c1dcce6e9b26e9f2a128232e21469b5984219261
Finn Aarup Nielsen <faan@dtu.dk>
2015-02-03 19:25:57 +0100

    Fixed styling.

--- brede/api/watson.py
+++ brede/api/watson.py
@@ -3,38 +3,40 @@
 
 from __future__ import print_function
 
-from ConfigParser import SafeConfigParser
+import json
 
 from itertools import islice
 
-import json
-
-from os.path import expanduser
+from brede.config import config
 
 import requests
 
 import yaml
 
-from brede.config import config
-
 
 class WatsonException(Exception):
+
     """Any kind of exception from Watson."""
+
     pass
 
 
 class WatsonFailedError(WatsonException):
+
     """Status 'Failed' from API."""
+
     pass
 
 
 class WatsonMissingConfig(WatsonException):
+
     """Exception for missing watson section in config."""
+
     pass
 
 
 class WatsonResponse(dict):
-    
+
     """Represent a response from the IBM Watson API."""
 
     def to_yaml(self):
@@ -49,8 +51,8 @@ class WatsonResponse(dict):
             else:
                 text = ''
             print('{:5} : {}{}'.format(int(float(evidence['value'])*1000),
-                                     evidence['title'], 
-                                     text))
+                                       evidence['title'],
+                                       text))
 
 
 class Watson(object):
@@ -61,9 +63,9 @@ class Watson(object):
 
     Example
     -------
-    >>> try: 
+    >>> try:
     ...     api = Watson()
-    ... except WatsonMissingConfig: 
+    ... except WatsonMissingConfig:
     ...     # Watson not available, so we don't test for queries.
     ...     print(True)
     ... else:
@@ -79,15 +81,16 @@ class Watson(object):
         if user is None and password is None and url is None:
             self.check_config()
         self.user = user if user else config.get('watson', 'user')
-        self.password = password if password else config.get('watson', 'password')
+        self.password = password if password else config.get('watson',
+                                                             'password')
         self.url = url if url else config.get('watson', 'url')
-        self.headers = {'Content-type': 'application/json', 
+        self.headers = {'Content-type': 'application/json',
                         'Accept': 'application/json'}
 
     def check_config(self):
         """Check the configuration file for credentials to Watson.
 
-        The section in the configuration file should contain user, password 
+        The section in the configuration file should contain user, password
         and url:
 
         [watson]
@@ -96,9 +99,9 @@ class Watson(object):
         url = <URL to API>
 
         """
-       
         if not config.has_section('watson'):
-            raise WatsonMissingConfig("Missing [watson] section in the config file.")
+            message = "Missing [watson] section in the config file."
+            raise WatsonMissingConfig(message)
 
     def ask(self, question, items=5):
         """Query the IBM Watson with a question.
@@ -112,14 +115,14 @@ class Watson(object):
         -------
         response : WatsonResponse
             Dict-like structure.
-        
+
         """
         data = {"question": {"questionText": question,
                              "items": items}}
-        response = requests.post(self.url, 
-                         headers=self.headers, 
-                         data=json.dumps(data), 
-                         auth=(self.user, self.password)).json()
+        response = requests.post(self.url,
+                                 headers=self.headers,
+                                 data=json.dumps(data),
+                                 auth=(self.user, self.password)).json()
         if response['question']['status'] == 'Failed':
             raise WatsonFailedError("'Failed' returned from IBM Watson API.")
         return WatsonResponse(response)
