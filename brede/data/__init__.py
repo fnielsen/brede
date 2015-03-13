dc535f11c8c894d8e21006c2f51b2c913acaa859
Finn Aarup Nielsen <faan@dtu.dk>
2015-03-13 15:34:15 +0100

    Added bredewiki module.

--- brede/data/__init__.py
+++ brede/data/__init__.py
@@ -2,11 +2,11 @@
 
 
 from brede.data import eeg, neurosynth, pubmed, wikipedia
+from brede.data.bredewiki import BredeWikiTemplates
 from brede.data.neurosynth import NeurosynthDatabase
 from brede.data.pubmed import Pubmed
 from brede.data.wikipedia import WikiPage
 
-
-__all__ = ('eeg',
+__all__ = ('BredeWikiTemplates', 'eeg',
            'neurosynth', 'NeurosynthDatabase',
            'pubmed', 'Pubmed', 'wikipedia', 'WikiPage')
