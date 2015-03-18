49c1353ae296a2e5ac8245a35b7229a04df6cb6b
Finn Aarup Nielsen <faan@dtu.dk>
2015-03-18 18:23:54 +0100

    Added Matrix as name in namespace.

--- brede/__init__.py
+++ brede/__init__.py
@@ -7,5 +7,7 @@ from . import eeg
 from . import io
 from .eeg import EegRun, topoplot
 from .io import read_edf
+from brede.core.matrix import Matrix
 
-__all__ = ('eeg', 'io', 'EegRun', 'read_edf', 'topoplot')
+
+__all__ = ('eeg', 'io', 'EegRun', 'Matrix', 'read_edf', 'topoplot')
