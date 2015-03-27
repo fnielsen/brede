b3c846a60c6c6a3b9454b0514a2d749f927bd2c1
Finn Aarup Nielsen <faan@dtu.dk>
2015-03-27 12:09:20 +0100

    Added reading of obj files.

--- brede/surface/core.py
+++ brede/surface/core.py
@@ -1,8 +1,13 @@
 """Handle surfaces."""
 
 
+from __future__ import division, print_function
+
+from brede.core.matrix import Matrix
+
 from mayavi.mlab import triangular_mesh
 
+import numpy as np
 
 class Surface(object):
 
@@ -13,6 +18,43 @@ class Surface(object):
         self._vertices = vertices
         self._faces = faces
         self._vertex_values = vertex_values
+
+    @classmethod
+    def read_obj(cls, filename):
+        """Read Wavefront obj file.
+
+        Only faces and vertices are read from the Wavefront file.
+
+        Arguments
+        ---------
+        filename : str
+            Filename for Wavefront file.
+
+        Returns
+        -------
+        surface : Surface
+            Surface object with the read surface.
+
+        """
+        vertices = []
+        faces = []
+        with open(filename) as fid: 
+            for line in fid:
+                elements = line.split()
+                if elements[0] == 'v':
+                    vertices.append([float(element) 
+                                     for element in elements[1:]])
+                elif elements[0] == 'vn': 
+                    # TODO
+                    pass
+                elif elements[0] == 'f':
+                    faces.append([int(element.split('//')[0]) 
+                                  for element in elements[1:]])
+                else:
+                    # TODO
+                    pass
+        return cls(np.array(vertices), np.array(faces) - 1)
+
 
     def plot(self, *args, **kwargs):
         """Plot surface."""
