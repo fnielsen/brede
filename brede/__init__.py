9358e38f39a72fb6cbfaa71825c937662d3a2687
Finn Aarup Nielsen <faan@dtu.dk>
2015-04-09 22:36:36 +0200

    Style fix and export fix.

--- brede/__init__.py
+++ brede/__init__.py
@@ -6,10 +6,11 @@ from __future__ import absolute_import
 from . import eeg
 from . import io
 from . import surface
+from .core.matrix import Matrix
 from .eeg import EegRun
 from .io import read_edf
-from .core.matrix import Matrix
 from .surface.core import Surface
 
 
-__all__ = ('eeg', 'io', 'EegRun', 'Matrix', 'read_edf', 'Surface', 'topoplot')
+__all__ = ('eeg', 'io', 'EegRun', 'Matrix', 'read_edf',
+           'surface', 'Surface', 'topoplot')
