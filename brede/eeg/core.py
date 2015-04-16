b491feee18e213c838e3f2dedfaa24c13b93e06d
Finn Aarup Nielsen <faan@dtu.dk>
2015-04-16 17:04:30 +0200

    Added emotiv_to_emocap change. Added Spectrum.

--- brede/eeg/core.py
+++ brede/eeg/core.py
@@ -1,9 +1,12 @@
 """Core data structures for EEG data."""
 
+
 from __future__ import absolute_import, division, print_function
 
 # Absolute path here because of circular dependency
 import brede.io
+
+import matplotlib.pyplot as plt
 
 import numpy as np
 
@@ -11,9 +14,34 @@ from pandas import DataFrame
 from pandas import read_csv as pandas_read_csv
 
 
-class UnevenSamplingRate(Exception):
+EMOTIV_TO_EMOCAP_MAP = {
+    'F3': 'P4',
+    'FC6': 'Fz',
+    'P7': 'TP10',
+    'T8': 'C3',
+    'F7': 'O1',
+    'F8': 'F4',
+    'T7': 'P3',
+    'P8': 'Cz',
+    'AF4': 'Fpz',
+    'F4': 'F3',
+    'AF3': 'O2',
+    'O2': 'C4',
+    'O1': 'TP9',
+    'FC5': 'Pz',
+}
+
+
+class UnevenSamplingRateError(Exception):
 
     """Exception for uneven sampling intervals."""
+
+    pass
+
+
+class ElectrodeNameError(Exception):
+
+    """Exception for wrong electrode name."""
 
     pass
 
@@ -39,13 +67,20 @@ class EEGRun(DataFrame):
         if sampling_rate is not None:
             self.index = np.arange(0, len(self) * sampling_rate, sampling_rate)
 
+    @property
     def sampling_rate(self):
-        """Return sampling rate."""
+        """Return sampling rate.
+
+        Raises
+        ------
+        UnevenSamplingRateError
+
+        """
         intervals = np.diff(self.index.values)
         interval = intervals.mean()
         interval_variation = max(intervals - interval) / interval
         if interval_variation > 10 ** -10:
-            raise UnevenSamplingRate
+            raise UnevenSamplingRateError
         return 1 / interval
 
     @classmethod
@@ -83,13 +118,44 @@ class EEGRun(DataFrame):
         """
         return cls(brede.io.read_edf(filename))
 
+    def emotiv_to_emocap(self, check_all=True, change_qualities=True,
+                         inplace=True):
+        """Change column names for Emotiv electrodes to Emocap.
+
+        Specialized method to change the electrode names in the dataframe
+        column from Emotiv electrode names to Emocap electrode names.
+
+        """
+        emotiv_to_emocap_map = EMOTIV_TO_EMOCAP_MAP
+
+        if check_all:
+            for column in emotiv_to_emocap_map:
+                if column not in self.columns:
+                    message = "Electrode '{}' not in columns".format(column)
+                    raise ElectrodeNameError(message)
+
+        columns = []
+        for column in self.columns:
+            if column in emotiv_to_emocap_map:
+                columns.append(emotiv_to_emocap_map[column])
+            elif (change_qualities and column.endswith(' quality') and
+                  column[:-8] in emotiv_to_emocap_map):
+                columns.append(emotiv_to_emocap_map[column[:-8]] + ' quality')
+            else:
+                columns.append(column)
+
+        if inplace:
+            self.columns = columns
+        else:
+            return EEGRun(self, columns=columns)
+
     def fft(self):
         """Fourier transform of data.
 
         Returns
         -------
-        df : pandas.DataFrame
-            Dataframe with frequencies in rows and electrodes in columns.
+        df : Spectrum
+            Dataframe-like with frequencies in rows and electrodes in columns.
 
         Examples
         --------
@@ -100,8 +166,85 @@ class EEGRun(DataFrame):
 
         """
         fourier = np.fft.fft(self, axis=0)
-        frequencies = np.fft.fftfreq(self.shape[0], 1 / self.sampling_rate())
-        return DataFrame(fourier, index=frequencies, columns=self.columns)
+        frequencies = np.fft.fftfreq(self.shape[0], 1 / self.sampling_rate)
+        return Spectrum(fourier, index=frequencies, columns=self.columns)
 
 
 read_csv = EEGRun.read_csv
+
+
+class Spectrum(DataFrame):
+
+    """Represent a spectrum for an EEG signal as a dataframe-like object."""
+
+    def plot_electrode_spectrum(self, electrode):
+        """Plot the spectrum of an electrode.
+
+        Parameters
+        ----------
+        electrode : str
+            Electrode name associated with column
+
+        """
+        positive_frequencies = self.index >= 0
+        plt.plot(self.index[positive_frequencies],
+                 np.abs(self.ix[positive_frequencies, electrode]))
+        plt.xlabel('Frequency')
+        plt.ylabel('Magnitude')
+        plt.title('Spectrum of {}'.format(electrode))
+
+    def plot_mean_spectrum(self):
+        """Plot the spectrum of the mean across electrodes.
+
+        Only the positive part of the spectrum is shown.
+
+        """
+        positive_frequencies = self.index >= 0
+        plt.plot(self.index[positive_frequencies],
+                 np.mean(np.abs(self.ix[positive_frequencies, :]), axis=1))
+        plt.xlabel('Frequency')
+        plt.ylabel('Magnitude')
+        plt.title('Mean spectrum across {} electrodes'.format(self.shape[1]))
+
+    def peak_frequency(self, min_frequency=0.0, max_frequency=None,
+                       electrode=None):
+        """Return frequency with peak magnitude.
+
+        Parameters
+        ----------
+        min_frequency : float
+            Minimum frequency to search for peak from
+        max_frequency : float
+            Maximum frequency to search for peak from
+        electrode : str, optional
+            Electrode to use for finding peak. If None then the mean of the
+            magnitude across all electrodes is used.
+
+        Returns
+        -------
+        freq : float
+            Peak frequency
+
+        Examples
+        --------
+        >>> spectrum = Spectrum({'Cz': [1, 2, 3, 1, 2, 10, 3, 4, 1]})
+        >>> spectrum.peak_frequency()
+        5
+
+        """
+        min_frequency = max(0.0, min_frequency)
+        if max_frequency is None:
+            max_frequency = max(self.index)
+
+        indices = (self.index >= min_frequency) & (self.index <= max_frequency)
+        if electrode is None:
+            magnitudes = np.mean(np.abs(self.ix[indices, :]), axis=1)
+        else:
+            magnitudes = np.abs(self.ix[indices, electrode])
+
+        peak_frequency = magnitudes.argmax()
+        return peak_frequency
+
+    def show(self):
+        """Show Matplotlib plot."""
+        plt.show()
