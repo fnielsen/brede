2561f4a18f9cd2de03fe18e2b1c7e202cab72ccc
Finn Aarup Nielsen <faan@dtu.dk>
2015-04-27 19:55:39 +0200

    Initial version.

---
+++ brede/core/tensor4d.py
@@ -0,0 +1,15 @@
+"""Panel4-like object."""
+
+
+from __future__ import absolute_import, division
+
+from pandas import Panel4D
+
+
+class Tensor4D(Panel4D):
+
+    """Extended panel4D object."""
+
+    @property
+    def _constructor(self):
+        return type(self)
