d6c0abefab75f7fa3a5d2afabdd861d794b14b3f
Finn Aarup Nielsen <faan@dtu.dk>
2015-04-27 19:53:04 +0200

    Several more tests.

--- brede/data/tests/test_keirnaunon.py
+++ brede/data/tests/test_keirnaunon.py
@@ -12,12 +12,19 @@ def test_name():
     assert ka.name == "1989 Keirn and Aunon"
 
 
+def test_sampling_rate():
+    assert keirnaunon.SAMPLING_RATE == 250
+
+
 def test_trial():
     """Test of trial method."""
     ka = keirnaunon.KeirnAunon()
 
-    # First element
     eeg_run = ka.trial()
+
+    assert eeg_run.sampling_rate == 250
+
+    # First element
     assert eeg_run.ix[0, 0] == 1.345
 
     eeg_run = ka.trial(trial=2)
@@ -28,3 +35,49 @@ def test_trial():
 
     eeg_run = ka.trial(2, 'counting', 3)
     assert abs(-20.507 - eeg_run.ix[0, 0]) < 0.0000001
+
+
+def test_trials_for_subject_state():
+    """Test of trials_for_subject_state method."""
+    ka = keirnaunon.KeirnAunon()
+
+    eeg_runs = ka.trials_for_subject_state()
+
+    assert eeg_runs.sampling_rate == 250
+
+    assert eeg_runs.shape[0] == 10
+    assert eeg_runs[1, 0.0, 'C3'] == 1.345
+    assert eeg_runs[1, 0.0, 'O2'] == 3.006
+    assert eeg_runs[5, 0.0, 'O2'] == 0.301
+
+    eeg_runs = ka.trials_for_subject_state(subject=1, state='baseline')
+    assert eeg_runs.shape[0] == 10
+    assert eeg_runs[1, 0.0, 'C3'] == 1.345
+    assert eeg_runs[1, 0.0, 'O2'] == 3.006
+    assert eeg_runs[5, 0.0, 'O2'] == 0.301
+
+    eeg_runs = ka.trials_for_subject_state(subject=7, state='counting')
+    assert eeg_runs.shape[0] == 5
+    assert eeg_runs[5, 0.0, 'C3'] == -6.204
+    assert eeg_runs[5, 0.0, 'O2'] == -1.482
+    assert eeg_runs[5, :, 'O2'].iloc[-1] == 13.341
+
+
+def test_trials_for_subject():
+    """Test of trials_for_subject method."""
+    ka = keirnaunon.KeirnAunon()
+
+    eeg_runs = ka.trials_for_subject()
+
+    assert eeg_runs.sampling_rate == 250
+
+    assert eeg_runs.shape[0] == 5
+    assert eeg_runs['baseline', 1, 0.0, 'C3'] == 1.345
+
+
+def test_peak_frequency():
+    """Test peak frequency finding for dataset."""
+    ka = keirnaunon.KeirnAunon()
+
+    eeg_runs = ka.trials_for_subject()
+    eeg_runs.fft().peak_frequency(min_frequency=5.0) == 60.0
