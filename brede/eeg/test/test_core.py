6b057feb07a52fdead49d52b5e2da9d4ac283ebd
Finn Aarup Nielsen <faan@dtu.dk>
2015-04-29 14:41:18 +0200

    Constructor test for new data.

--- brede/eeg/test/test_core.py
+++ brede/eeg/test/test_core.py
@@ -3,6 +3,8 @@
 
 from __future__ import absolute_import
 
+import numpy as np
+
 from .. import core
 
 
@@ -22,6 +24,13 @@ def test_eegrun():
     assert eeg_run.ix[0.5, 'C4'] == 4
 
 
+def test_eegrun_constructor():
+    """Test result constructur."""
+    eeg_run = core.EEGRun([[1, 2], [3, 4]], columns=['C3', 'C4'],
+                          sampling_rate=2.0)
+    assert isinstance(np.isnan(eeg_run), core.EEGRun)
+
+
 def test_eegruns():
     """Test EEGRuns."""
     eeg_runs = core.EEGRuns([[[1, 2], [3, 4]]],
@@ -34,6 +43,15 @@ def test_eegruns():
 
     assert eeg_runs['Trial 1', 0.0, 'C3'] == 1
     assert eeg_runs['Trial 1', 0.5, 'C4'] == 4
+
+
+def test_eegruns_constructor():
+    """Test result constructur."""
+    eeg_runs = core.EEGRuns([[[1, 2], [3, 4]]],
+                            items=['Trial 1'],
+                            minor_axis=['C3', 'C4'],
+                            sampling_rate=2.0)
+    assert isinstance(np.isnan(eeg_runs), core.EEGRuns)
 
 
 def test_eegruns4d():
@@ -49,3 +67,12 @@ def test_eegruns4d():
 
     assert eeg_runs['baseline', 'Trial 1', 0.0, 'C3'] == 1
     assert eeg_runs['baseline', 'Trial 1', 0.5, 'C4'] == 4
+
+
+def test_eegruns4d_constructor():
+    """Test result constructur."""
+    eeg_runs4d = core.EEGRuns4D(
+        [[[[1, 2], [3, 4]]]],
+        labels=['baseline'], items=['Trial 1'],
+        minor_axis=['C3', 'C4'], sampling_rate=2.0)
+    assert isinstance(np.isnan(eeg_runs4d), core.EEGRuns4D)
