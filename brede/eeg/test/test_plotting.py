872d053a48adb07723bc67f67ba87e915e12e367
Finn Aarup Nielsen <faan@dtu.dk>
2015-05-05 18:59:26 +0200

    Docstrings.

--- brede/eeg/test/test_plotting.py
+++ brede/eeg/test/test_plotting.py
@@ -2,12 +2,14 @@
 
 
 import matplotlib.pyplot as plt
+
 import pandas as pd
 
 from .. import plotting
 
 
 def test_topoplot_empty():
+    """Test topoplot function for empty input."""
     plotting.topoplot()
     plt.ion()
     plt.show()
@@ -15,6 +17,7 @@ def test_topoplot_empty():
 
 
 def test_topoplot_series():
+    """Test topoplot function for Series-like input."""
     data = {'C3': 4, 'Fz': 3, 'C4': 5}
     plotting.topoplot(data)
     plt.ion()
@@ -23,6 +26,7 @@ def test_topoplot_series():
 
 
 def test_topoplot_dataframe():
+    """Test topoplot function for dataframe-like input."""
     data = pd.DataFrame({'C3': [4, 2], 'Fz': [3, 3], 'C4': [5, 3]})
     plotting.topoplot(data)
     plt.ion()
