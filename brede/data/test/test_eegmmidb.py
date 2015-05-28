b481e73369cbe0d6a4f5cd8767d78e5c66de5bb5
Finn Aarup Nielsen <faan@dtu.dk>
2015-05-28 09:36:42 +0200

    Initial version.

---
+++ brede/data/test/test_eegmmidb.py
@@ -0,0 +1,28 @@
+"""Test of eegmmidb."""
+
+
+from __future__ import absolute_import, division, print_function
+
+from brede.eeg.core import EEGAuxRun
+
+import pytest
+
+from ..eegmmidb import EEGMMIDB
+
+
+@pytest.fixture
+def eegmmidb():
+    eegmmidb = EEGMMIDB()
+    return eegmmidb
+
+
+def test_run(eegmmidb):
+    """Test run method."""
+    run = eegmmidb.run()
+    assert isinstance(run, EEGAuxRun)
+
+    run11 = eegmmidb.run(1, 1)
+    assert (run11 == run).all().all()
+
+    run11 = eegmmidb.run(run=1, subject=1)
+    assert (run11 == run).all().all()
