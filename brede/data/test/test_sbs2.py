8d0171a99940d3c80f04b76e090dc1ed7f30267c
Finn Aarup Nielsen <faan@dtu.dk>
2015-06-02 00:45:38 +0200

    Added tests.

--- brede/data/test/test_sbs2.py
+++ brede/data/test/test_sbs2.py
@@ -31,3 +31,29 @@ def test_electrode_names(sbs2_data):
 def test_name(sbs2_data):
     """Test name."""
     assert sbs2_data.name == "SBS2 data files"
+
+
+def test_forward_model(sbs2_data):
+    """Test forward model."""
+    forward = sbs2_data.forward_model()
+    assert forward.shape == (1028, 14)
+
+
+def test_inverse_model(sbs2_data):
+    """Test inverse model."""
+    inverse = sbs2_data.inverse_model()
+    assert inverse.shape == (14, 1028)
+
+    inverse = sbs2_data.inverse_model(method='minimumnorm')
+    assert inverse.shape == (14, 1028)
+
+    inverse = sbs2_data.inverse_model(method='LORETA')
+    assert inverse.shape == (14, 1028)
+
+
+def test_spatial_coherence(sbs2_data):
+    """Test spatial coherence."""
+    coherence = sbs2_data.spatial_coherence()
+
+    # Symmetry
+    assert (coherence.T == coherence).all().all()
