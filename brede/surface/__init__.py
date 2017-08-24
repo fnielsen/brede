9e9556db1d252dcb97253f5ba5799bbd996611ae
fnielsen <fn@imm.dtu.dk>
2017-08-24 21:47:00 +0200

    Add read_mat as an available name

--- brede/surface/__init__.py
+++ brede/surface/__init__.py
@@ -2,7 +2,7 @@
 
 from __future__ import absolute_import
 
-from .core import Surface, read_obj
+from .core import Surface, read_mat, read_obj
 
 
-__all__ = ('read_obj', 'Surface')
+__all__ = ('read_mat', 'read_obj', 'Surface')
