3f20ae4e697dd49fc29d98cccf89730ee533745d
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-21 11:56:57 +0000

    Adding tarfile member sanitization to extractall()

--- brede/data/neurosynth.py
+++ brede/data/neurosynth.py
@@ -119,7 +119,29 @@ class NeurosynthDatabase(Data):
             try:
                 with tarfile.open(self.neurosynth_download_filename,
                                   'r:gz') as fid:
-                    fid.extractall()
+                    
+                    import os
+                    
+                    def is_within_directory(directory, target):
+                        
+                        abs_directory = os.path.abspath(directory)
+                        abs_target = os.path.abspath(target)
+                    
+                        prefix = os.path.commonprefix([abs_directory, abs_target])
+                        
+                        return prefix == abs_directory
+                    
+                    def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                    
+                        for member in tar.getmembers():
+                            member_path = os.path.join(path, member.name)
+                            if not is_within_directory(path, member_path):
+                                raise Exception("Attempted Path Traversal in Tar File")
+                    
+                        tar.extractall(path, members, numeric_owner=numeric_owner) 
+                        
+                    
+                    safe_extract(fid)
             finally:
                 chdir(cwd)
 
